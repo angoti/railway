986fa89bb0627ab2b1ce8d3b7e00ed2e85c41b10
angoti <angoti@gmail.com>
2025-08-22 12:00:05 -0300

    controlador /hello

---
+++ src/main/java/edu/iftm/demo/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package edu.iftm.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String sayHello() {
+        return "Hello, World!";
+    }
+}
